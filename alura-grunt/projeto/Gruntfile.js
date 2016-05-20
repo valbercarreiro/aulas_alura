ffc00e9fa45d36818cd6d1425216dbec632e2f2a
valbercarreiro <valbercarreiro@gmail.com>
2016-05-20 16:17:21 -0300

    --all

--- alura-grunt/projeto/Gruntfile.js
+++ alura-grunt/projeto/Gruntfile.js
@@ -20,12 +20,35 @@ module.exports = function(grunt) {
 
       usemin: {
           html: 'dist/**/*.html'
-        }
+        },
+      imagemin: {
+         public: {
+           expand: true,
+           cwd: 'dist/img',
+           src: '**/*.{png,jpg,gif}',
+           dest: 'dist/img'
+         }
+      },
+      rev: {
+          options: {
+            encoding: 'utf8',
+            algorithm: 'md5',
+            length: 8
+          },
+
+          imagens: {
+            src: ['dist/img/**/*.{png,jpg,gif}']
+          },
+
+          minificados: {
+            src: ['dist/js/**/*.min.js', 'dist/css/**/*.min.css']
+          }
+    }
   });
 
   grunt.registerTask('dist', ['clean', 'copy']);
   grunt.registerTask('minifica', ['useminPrepare',
-                                  'concat', 'uglify', 'cssmin', 'usemin']);
+                                  'concat', 'uglify', 'cssmin', 'rev:imagens','rev:minificados', 'usemin', 'imagemin']);
   grunt.registerTask('default', ['dist','minifica']);
 
   grunt.loadNpmTasks('grunt-contrib-copy');
@@ -34,4 +57,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-usemin');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-rev');
 }
