75f12b1a3d1dabb1d2d77b8587caed598dd2797e
valbercarreiro <valbercarreiro@gmail.com>
2016-06-01 13:10:16 -0300

    alura angualrjs

--- alura-angularjs/alurapic/public/js/controllers/fotos-controller.js
+++ alura-angularjs/alurapic/public/js/controllers/fotos-controller.js
@@ -1,6 +1,11 @@
-angular.module('alurapic').controller('FotosController',function($scope){
-	$scope.foto = {
-		titulo: "Leão",
-		url: "http://www.fundosanimais.com/Minis/leoes.jpg"
-	};
+angular.module('alurapic').controller('FotosController',function($scope, $http){
+	$scope.fotos = [];
+
+	$http.get('v1/fotos')
+	.success(function(fotos){
+		$scope.fotos = fotos;
+	}).error(function(error){
+		console.log(error);
+	});
+
 });
