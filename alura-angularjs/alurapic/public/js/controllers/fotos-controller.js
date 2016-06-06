b4c6021179bbeac77b24b61a5af2b5e155f080a6
valbercarreiro <valbercarreiro@gmail.com>
2016-06-06 13:20:31 -0300

    aula angularjs

--- alura-angularjs/alurapic/public/js/controllers/fotos-controller.js
+++ alura-angularjs/alurapic/public/js/controllers/fotos-controller.js
@@ -1,11 +1,12 @@
 angular.module('alurapic').controller('FotosController',function($scope, $http){
-	$scope.fotos = [];
+    $scope.fotos = [];
+    $scope.filtro = '';
 
-	$http.get('v1/fotos')
-	.success(function(fotos){
-		$scope.fotos = fotos;
-	}).error(function(error){
-		console.log(error);
-	});
+    $http.get('v1/fotos')
+    .success(function(fotos){
+        $scope.fotos = fotos;
+    }).error(function(error){
+        console.log(error);
+    });
 
 });
