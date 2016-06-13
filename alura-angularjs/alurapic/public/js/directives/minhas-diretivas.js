1071e19151737b906ef0a49808e66d90d5e6d435
valbercarreiro <valbercarreiro@gmail.com>
2016-06-13 15:49:04 -0300

    aula angular

--- alura-angularjs/alurapic/public/js/directives/minhas-diretivas.js
+++ alura-angularjs/alurapic/public/js/directives/minhas-diretivas.js
@@ -30,4 +30,20 @@ angular.module('minhasDiretivas', [])
 
     return ddo;
 
+})
+.directive('meuBotaoPerigo', function() {
+
+    var ddo = {};
+
+    ddo.restrict = "E";
+
+    ddo.scope = {
+        nome: '@',
+        acao: '&'
+    };
+
+    ddo.template = '<button ng-click="acao(foto)" class="btn btn-danger btn-block">{{nome}}</button>';
+
+    return ddo;
+
 });
