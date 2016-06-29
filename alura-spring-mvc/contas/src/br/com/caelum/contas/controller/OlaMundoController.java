d0572be3ae6e19bcba15549091b49a912daf037d
valbercarreiro <valbercarreiro@gmail.com>
2016-06-29 13:41:48 -0300

    alura spring mvc

---
+++ alura-spring-mvc/contas/src/br/com/caelum/contas/controller/OlaMundoController.java
@@ -0,0 +1,16 @@
+package br.com.caelum.contas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OlaMundoController {
+
+	@RequestMapping("/olaMundoSpring")
+	public String executar() {
+		System.out.println("Olá mundo com spring mvc!");
+		
+		return "ok";
+
+	}
+}
