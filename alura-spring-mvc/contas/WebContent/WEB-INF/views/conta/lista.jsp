426129a16c0d81ef12f1b538950673f1078f6ca9
valbercarreiro <valbercarreiro@gmail.com>
2016-07-04 16:05:58 -0300

    spring mvc

---
+++ alura-spring-mvc/contas/WebContent/WEB-INF/views/conta/lista.jsp
@@ -0,0 +1,44 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+
+	<table>
+		<tr>
+			<th>Código</th>
+			<th>Descrição</th>
+			<th>Valor</th>
+			<th>Tipo</th>
+			<th>Paga?</th>
+			<th>Data de Pagamento</th>
+		</tr>
+		
+		<c:forEach items="${todasContas}" var="conta">
+			<tr>
+				<td>${conta.id}</td>
+				<td>${conta.descricao}</td>
+				<td>${conta.valor}</td>
+				<td>${conta.tipo}</td>
+				<td>
+					<c:if test="${conta.paga eq false}">
+						Não Paga
+					</c:if>
+				 	<c:if test="${conta.paga eq true}">
+				 		Paga
+				 	</c:if>
+				</td>
+				<td><fmt:formatDate value="${conta.dataPagamento.time}" pattern="dd/MM/yyyy"/></td>
+			</tr>
+		</c:forEach>
+	
+	</table>
+
+</body>
+</html>
--- alura-spring-mvc/contas/src/br/com/caelum/contas/controller/ContaController.java
+++ alura-spring-mvc/contas/src/br/com/caelum/contas/controller/ContaController.java
@@ -1,7 +1,11 @@
 package br.com.caelum.contas.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.caelum.contas.dao.ContaDAO;
 import br.com.caelum.contas.modelo.Conta;
@@ -17,11 +21,31 @@ public class ContaController {
 
 	}
 
-	@RequestMapping("/conta/adicionarConta")
+	@RequestMapping("/adicionarConta")
 	public String adiciona(Conta c){
 		System.out.println("Conta adicionada é: "+c.getDescricao());
 		ContaDAO dao = new ContaDAO();
 		dao.adiciona(c);
 		return "conta/conta-adicionada";
+	}
+	
+//	@RequestMapping("/listaContas")
+//	public ModelAndView lista(){
+//		ContaDAO dao = new ContaDAO();
+//		List<Conta> contas = dao.lista();
+//		
+//		ModelAndView mv = new ModelAndView("conta/lista");
+//		mv.addObject("todasContas", contas);
+//		
+//		return mv;
+//	}
+	@RequestMapping("/listaContas")
+	public String lista(Model mv){
+		ContaDAO dao = new ContaDAO();
+		List<Conta> contas = dao.lista();
+		
+		mv.addAttribute("todasContas", contas);
+		
+		return "conta/lista";
 	}
 }
