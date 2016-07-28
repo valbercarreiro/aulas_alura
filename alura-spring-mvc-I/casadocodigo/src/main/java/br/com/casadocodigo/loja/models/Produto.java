8b1df6698e4045e914f4e1e518cd45cb135cbba5
valbercarreiro <valbercarreiro@gmail.com>
2016-07-28 11:21:30 -0300

    alura - spring mvc I

--- alura-spring-mvc-I/casadocodigo/src/main/java/br/com/casadocodigo/loja/models/Produto.java
+++ alura-spring-mvc-I/casadocodigo/src/main/java/br/com/casadocodigo/loja/models/Produto.java
@@ -1,5 +1,6 @@
 package br.com.casadocodigo.loja.models;
 
+import java.util.Calendar;
 import java.util.List;
 
 import javax.persistence.ElementCollection;
@@ -7,6 +8,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 public class Produto {
@@ -20,6 +23,9 @@ public class Produto {
 	private String descricao;
 	
 	private int paginas;
+	
+	@DateTimeFormat
+	private Calendar dataLancamento;
 	
 	@ElementCollection
 	private List<Preco> precos;
@@ -64,9 +70,16 @@ public class Produto {
 		this.precos = precos;
 	}
 	
+	public Calendar getDataLancamento() {
+		return dataLancamento;
+	}
+	
+	public void setDataLancamento(Calendar dataLancamento) {
+		this.dataLancamento = dataLancamento;
+	}
+	
 	@Override
 	public String toString() {
 		return "Produto [titulo=" + titulo + ", descricao=" + descricao + ", paginas=" + paginas + "]";
-	}
-	
+	}	
 }
