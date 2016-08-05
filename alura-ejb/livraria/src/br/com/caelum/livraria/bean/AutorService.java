40aa63f2a0a03353064f19c8bb743fe3787181a2
valbercarreiro <valbercarreiro@gmail.com>
2016-08-05 14:56:33 -0300

    alura - Curso EJB: O poder da Java EE

---
+++ alura-ejb/livraria/src/br/com/caelum/livraria/bean/AutorService.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package br.com.caelum.livraria.bean;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import br.com.caelum.livraria.dao.AutorDao;
+import br.com.caelum.livraria.modelo.Autor;
+
+/**
+ * @author Valber Paulino
+ *
+ */
+@Stateless
+public class AutorService {
+
+	@Inject
+	AutorDao autorDao;
+	
+	public void adiciona(Autor autor){
+		//mais regras aqui
+		
+		autorDao.salva(autor);
+	}
+	
+	public List<Autor> todosAutores() {
+		return autorDao.todosAutores();
+	}
+}
--- alura-ejb/livraria/src/br/com/caelum/livraria/dao/AutorDao.java
+++ alura-ejb/livraria/src/br/com/caelum/livraria/dao/AutorDao.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -20,14 +22,9 @@ public class AutorDao {
 		System.out.println("AutorDao foi criado");
 	}
 
+	@TransactionAttribute(TransactionAttributeType.MANDATORY)
 	public void salva(Autor autor) {
 		System.out.println("Salvando autor "+autor.getNome());
-		
-//		try {
-//			Thread.sleep(20000);
-//		} catch (InterruptedException e) {
-//			e.printStackTrace();
-//		}
 		
 		manager.persist(autor);
 		
