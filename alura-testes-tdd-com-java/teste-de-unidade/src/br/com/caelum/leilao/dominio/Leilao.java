56f757f396997e1a8b0b8634531cbd48c21a8220
valbercarreiro <valbercarreiro@gmail.com>
2016-09-12 22:41:04 -0300

    Curso Testes: TDD com Java

--- alura-testes-tdd-com-java/teste-de-unidade/src/br/com/caelum/leilao/dominio/Leilao.java
+++ alura-testes-tdd-com-java/teste-de-unidade/src/br/com/caelum/leilao/dominio/Leilao.java
@@ -15,7 +15,27 @@ public class Leilao {
 	}
 	
 	public void propoe(Lance lance) {
-		lances.add(lance);
+		if(lances.isEmpty() || podeDarLance(lance.getUsuario())){
+			lances.add(lance);
+		}
+	}
+
+	private boolean podeDarLance(Usuario usuario) {
+		return !ultimoLanceDado().getUsuario().equals(usuario) && qntdadeLancesDo(usuario) < 5;
+	}
+
+	private int qntdadeLancesDo(Usuario usuario) {
+		int retorno = 0;
+		for (Lance lance2 : lances) {
+			if(lance2.getUsuario().equals(usuario)){
+				retorno ++;
+			}
+		}
+		return retorno;
+	}
+
+	private Lance ultimoLanceDado() {
+		return lances.get(lances.size()-1);
 	}
 
 	public String getDescricao() {
@@ -25,7 +45,20 @@ public class Leilao {
 	public List<Lance> getLances() {
 		return Collections.unmodifiableList(lances);
 	}
+	
+    private Lance ultimoLanceDo(Usuario usuario) {
+        Lance ultimo = null;
+        for(Lance lance : lances) {
+            if(lance.getUsuario().equals(usuario)) ultimo = lance;
+        }
 
-	
-	
+        return ultimo;
+    }
+    
+    public void dobraLance(Usuario usuario) {
+        Lance ultimoLance = ultimoLanceDo(usuario);
+        if(ultimoLance!=null) {
+            propoe(new Lance(usuario, ultimoLance.getValor()*2));
+        }
+    }
 }
