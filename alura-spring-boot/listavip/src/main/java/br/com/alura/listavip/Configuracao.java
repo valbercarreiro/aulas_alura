abf7117e594f99828c034be7a08946905d185a3a
valbercarreiro <valbercarreiro@gmail.com>
2016-09-22 10:15:57 -0300

    spring boot - alura

---
+++ alura-spring-boot/listavip/src/main/java/br/com/alura/listavip/Configuracao.java
@@ -0,0 +1,55 @@
+package br.com.alura.listavip;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@SpringBootApplication
+public class Configuracao {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Configuracao.class, args);
+	}
+
+	@Bean
+	public DataSource dataSource(){
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.postgresql.Driver");
+		dataSource.setUrl("jdbc:postgresql://localhost:5432/listavip");
+		dataSource.setUsername("postgres");
+		dataSource.setPassword("root");
+		return dataSource;
+	}
+	
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+			DataSource dataSource, Properties additionalProperties) {
+		LocalContainerEntityManagerFactoryBean factoryBean = 
+				new LocalContainerEntityManagerFactoryBean();
+		factoryBean.setPackagesToScan("br.com.alura.listavip.model");
+		factoryBean.setDataSource(dataSource);
+		
+		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		factoryBean.setJpaVendorAdapter(vendorAdapter);
+		factoryBean.setJpaProperties(additionalProperties);
+		
+		return factoryBean;
+	}
+
+	@Bean
+	public Properties additionalProperties() {
+		Properties props = new Properties();
+		props.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+		props.setProperty("hibernate.show_sql", "true");
+		props.setProperty("hibernate.hbm2ddl.auto", "update");
+		return props;
+	}
+}
