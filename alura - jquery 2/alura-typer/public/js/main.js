09d90a2a2551290e34d68153bb70282135b2980d
valbercarreiro <valbercarreiro@gmail.com>
2016-11-01 09:39:49 -0200

    alura - jquery dominando a lib mais popular do mercado

--- alura - jquery 2/alura-typer/public/js/main.js
+++ alura - jquery 2/alura-typer/public/js/main.js
@@ -4,3 +4,10 @@ var contadorDePalavras = function(){
     $("#tamanho-frase").text(paragrafo.split(" ").length);
 };
 $(contadorDePalavras);
+
+var campo = $(".campo-digitacao");
+campo.on("input", function(){
+    $("#contador-caracteres").text(campo.val().length);
+
+    $("#contador-palavras").text(campo.val().split(/\S+/).length - 1);
+});
