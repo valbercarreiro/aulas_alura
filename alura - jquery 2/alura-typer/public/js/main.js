469ba87a868297ba27970a6f60aa08d0a4b48131
valbercarreiro <valbercarreiro@gmail.com>
2016-11-03 11:24:21 -0200

    alura - jquery dominando a lib mais popular do mercado

--- alura - jquery 2/alura-typer/public/js/main.js
+++ alura - jquery 2/alura-typer/public/js/main.js
@@ -1,25 +1,47 @@
+var campo = $(".campo-digitacao");
+
+$(function(){
+    $(contadorDePalavras);
+    $(inicializaContatodes);
+    $(inicializaCronometro);
+    $("#botao-reiniciar").click(reiniciaJogo);
+});
+
 var contadorDePalavras = function(){
     var paragrafo = $(".frase").text();
 
     $("#tamanho-frase").text(paragrafo.split(" ").length);
 };
-$(contadorDePalavras);
 
-var campo = $(".campo-digitacao");
-campo.on("input", function(){
-    $("#contador-caracteres").text(campo.val().length);
+var inicializaContatodes = function(){
+    campo.on("input", function(){
+        $("#contador-caracteres").text(campo.val().length);
 
-    $("#contador-palavras").text(campo.val().split(/\S+/).length - 1);
-});
+        $("#contador-palavras").text(campo.val().split(/\S+/).length - 1);
+    });
+};
 
-var tempoRestante = $("#tempo-digitacao").text();
-campo.one("focus", function(){
-    var cronometroID = setInterval(function(){
-        tempoRestante--;
-        $("#tempo-digitacao").text(tempoRestante);
-        if(tempoRestante < 1){
-            campo.attr("disabled", true);
-            clearInterval(cronometroID);
-        }
-    },1000);
-});
+var inicializaCronometro = function(){
+    var tempoRestante = $("#tempo-digitacao").text();
+    campo.one("focus", function(){
+        $("#botao-reiniciar").attr("disabled",true);
+        var cronometroID = setInterval(function(){
+            tempoRestante--;
+            $("#tempo-digitacao").text(tempoRestante);
+            if(tempoRestante < 1){
+                campo.attr("disabled", true);
+                clearInterval(cronometroID);
+                $("#botao-reiniciar").attr("disabled", false);
+            }
+        },1000);
+    });
+};
+
+var reiniciaJogo = function(){
+    campo.attr("disabled", false);
+    campo.val("");
+    $("#contador-palavras").text("0");
+    $("#contador-caracteres").text("0");
+    $("#tempo-digitacao").text("10");
+    $(inicializaCronometro);
+};
