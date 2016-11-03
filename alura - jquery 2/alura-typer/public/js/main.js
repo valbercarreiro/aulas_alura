96b836246ac5e412b58c14284601bea4065f4d0f
valbercarreiro <valbercarreiro@gmail.com>
2016-11-03 10:37:32 -0200

    alura - jquery dominando a lib mais popular do mercado

--- alura - jquery 2/alura-typer/public/js/main.js
+++ alura - jquery 2/alura-typer/public/js/main.js
@@ -11,3 +11,15 @@ campo.on("input", function(){
 
     $("#contador-palavras").text(campo.val().split(/\S+/).length - 1);
 });
+
+var tempoRestante = $("#tempo-digitacao").text();
+campo.one("focus", function(){
+    var cronometroID = setInterval(function(){
+        tempoRestante--;
+        $("#tempo-digitacao").text(tempoRestante);
+        if(tempoRestante < 1){
+            campo.attr("disabled", true);
+            clearInterval(cronometroID);
+        }
+    },1000);
+});
