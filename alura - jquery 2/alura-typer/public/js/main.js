1f8a7b72c82bdf243ccbc12036bd588a647ad1f7
valbercarreiro <valbercarreiro@gmail.com>
2016-11-03 14:59:34 -0200

    alura - jquery dominando a lib mais popular do mercado

--- alura - jquery 2/alura-typer/public/js/main.js
+++ alura - jquery 2/alura-typer/public/js/main.js
@@ -5,6 +5,7 @@ $(function(){
     $(inicializaContatodes);
     $(inicializaCronometro);
     $("#botao-reiniciar").click(reiniciaJogo);
+    $(inicializaMarcadores);
 });
 
 var contadorDePalavras = function(){
@@ -32,10 +33,27 @@ var inicializaCronometro = function(){
                 campo.attr("disabled", true);
                 clearInterval(cronometroID);
                 $("#botao-reiniciar").attr("disabled", false);
+                campo.toggleClass("campo-desativado");
             }
         },1000);
     });
 };
+
+var inicializaMarcadores = function(){
+    var frase = $(".frase").text();
+    campo.on("input", function(){
+        var digitado = campo.val();
+        var comparavel = frase.substr(0, digitado.length);
+
+        if(digitado == comparavel){
+            campo.removeClass("borda-vermelha");
+            campo.addClass("borda-verde");
+        } else {
+            campo.removeClass("borda-verde");
+            campo.addClass("borda-vermelha");
+        }
+    });
+}
 
 var reiniciaJogo = function(){
     campo.attr("disabled", false);
@@ -43,5 +61,8 @@ var reiniciaJogo = function(){
     $("#contador-palavras").text("0");
     $("#contador-caracteres").text("0");
     $("#tempo-digitacao").text("10");
+    campo.toggleClass("campo-desativado");
     $(inicializaCronometro);
+    campo.removeClass("borda-vermelha");
+    campo.removeClass("borda-verde");
 };
