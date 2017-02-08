f634ec6c401609b17981003f171c7362224d7028
valbercarreiro <valbercarreiro@gmail.com>
2017-02-08 10:39:54 -0200

    alura - javascript avancado

--- alura-javascript-avancado/javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ alura-javascript-avancado/javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -5,22 +5,28 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
     }
 
     adiciona(event){
         event.preventDefault();
-
-        //let data = new Date(this._inputData.value.replace(/-/g,'-'));
-        let data = new Date(...
-            this._inputData.value.split('-')
-            .map((item, indice) => item - indice%2)
-        );
-        let negociacao = new Negociacao(
-                            data, 
-                            this._inputQuantidade.value, 
-                            this._inputValor.value);
-        console.log(negociacao);
-
+        let negociacao = this._criaNegociacao();        
+        this._listaNegociacoes.adiciona(negociacao);
+        this._limpaFormulario();
     }
 
+    _criaNegociacao() {
+        return new Negociacao(
+                            DataHelper.textoParaData(this._inputData.value), 
+                            this._inputQuantidade.value, 
+                            this._inputValor.value
+                        );
+    }
+
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();
+    }
 }
