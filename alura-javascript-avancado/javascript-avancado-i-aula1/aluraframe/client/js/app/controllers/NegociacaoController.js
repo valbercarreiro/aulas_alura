a48fc118c11d721b2ebaedbcc3156925ca985628
valbercarreiro <valbercarreiro@gmail.com>
2017-02-09 15:25:13 -0200

    alura - javascript avancado

--- alura-javascript-avancado/javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ alura-javascript-avancado/javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -6,13 +6,17 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
         this._listaNegociacoes = new ListaNegociacoes();
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+
+        this._negociacoesView.update(this._listaNegociacoes);
     }
 
     adiciona(event){
         event.preventDefault();
         let negociacao = this._criaNegociacao();        
         this._listaNegociacoes.adiciona(negociacao);
-        this._limpaFormulario();
+        this._negociacoesView.update(this._listaNegociacoes);
+        this._limpaFormulario();        
     }
 
     _criaNegociacao() {
