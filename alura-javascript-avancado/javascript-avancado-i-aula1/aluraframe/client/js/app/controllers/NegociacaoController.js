56a67fa8300f0174a8609f748fb38bbe7d3b08c0
valbercarreiro <valbercarreiro@gmail.com>
2017-02-10 09:15:00 -0200

    alura - javascript avancado

--- alura-javascript-avancado/javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ alura-javascript-avancado/javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -7,15 +7,22 @@ class NegociacaoController {
         this._inputValor = $('#valor');
         this._listaNegociacoes = new ListaNegociacoes();
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+        this._mensagem = new Mensagem();
+        this._mensagemView = new MensagemView($('#mensagemView'));
 
         this._negociacoesView.update(this._listaNegociacoes);
+        this._mensagemView.update(this._mensagem);
     }
 
     adiciona(event){
         event.preventDefault();
         let negociacao = this._criaNegociacao();        
         this._listaNegociacoes.adiciona(negociacao);
+        this._mensagem.texto = 'Negociação adicionada com sucesso.';
+        
+        this._mensagemView.update(this._mensagem);
         this._negociacoesView.update(this._listaNegociacoes);
+        
         this._limpaFormulario();        
     }
 
--- alura-javascript-avancado/javascript-avancado-i-aula1/aluraframe/client/js/app/views/NegociacoesView.js
+++ alura-javascript-avancado/javascript-avancado-i-aula1/aluraframe/client/js/app/views/NegociacoesView.js
@@ -1,10 +1,10 @@
-class NegociacoesView {
+class NegociacoesView extends View {
 
     constructor(elemento) {
-        this._elemento = elemento;
+        super(elemento);
     }
 
-    _template(model) {
+    template(model) {
         return `
             <table class="table table-hover table-bordered">
                 <thead>
@@ -38,9 +38,4 @@ class NegociacoesView {
         `;
     }
 
-    update(model) {
-
-        this._elemento.innerHTML = this._template(model);
-
-    }
 }
