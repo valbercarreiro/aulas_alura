78d4ccee5afa0ab54cbab5f2afd65c8f5e99e6a5
valbercarreiro <valbercarreiro@gmail.com>
2017-02-14 15:54:45 -0200

    alura - javascript avancado

--- alura-javascript-avancado/javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ alura-javascript-avancado/javascript-avancado-i-aula1/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -5,7 +5,21 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes();
+        
+        let self = this;
+        this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
+            get(target, prop, receiver) {
+                if(['adiciona','esvazia'].includes(prop) && typeof(target[prop]) == typeof(Function)){
+                    return function(){
+                        console.log(`interceptando ${prop}`);
+                        Reflect.apply(target[prop], target, arguments);
+                        self._negociacoesView.update(target);
+                    }
+                }
+                return Reflect.get(target,prop,receiver);
+            }
+        });
+
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._mensagem = new Mensagem();
         this._mensagemView = new MensagemView($('#mensagemView'));
@@ -20,9 +34,7 @@ class NegociacaoController {
         this._listaNegociacoes.adiciona(negociacao);
         this._mensagem.texto = 'Negociação adicionada com sucesso.';
         
-        this._mensagemView.update(this._mensagem);
-        this._negociacoesView.update(this._listaNegociacoes);
-        
+        this._mensagemView.update(this._mensagem);        
         this._limpaFormulario();        
     }
 
@@ -39,5 +51,12 @@ class NegociacaoController {
         this._inputQuantidade.value = 1;
         this._inputValor.value = 0.0;
         this._inputData.focus();
+    }
+
+    apaga() {
+        this._listaNegociacoes.esvazia();
+        
+        this._mensagem.texto = 'Lista de Negociações apagadas com sucesso';
+        this._mensagemView.update(this._mensagem);
     }
 }
